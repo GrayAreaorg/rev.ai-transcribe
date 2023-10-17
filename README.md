9398ddfcfb0a4f706594dfc385aa76603aba8150
Gabriel Dunne <gdunne@quilime.com>
2023-10-16 19:01:16 -0700

    Building out front end

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Rev.AI Real Time Audio Stream Transcription
 
 A microsite for generating captions from an incoming audio stream.
+
+## Requirements
+
+Node v20.7.0
+
+## Install
+
+`npm install`
+
+## Run
+
+`node app.js`
+
+Open a browser and go to `localhost:3000`
+
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const revai = require('revai-node-sdk');
 require('dotenv').config()
 
@@ -19,6 +22,14 @@ const audioConfig = new revai.AudioConfig(
 
 var client = new revai.RevAiStreamingClient(token, audioConfig);
 
+// initialize express app
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// serve static files from 'public' directory
+app.use(express.static('public'));
+
 // create event responses
 client.on('close', (code, reason) => {
     console.log(`Connection closed, ${code}: ${reason}`);
@@ -39,7 +50,9 @@ var stream = client.start();
 // create event responses
 //this is the audio stream in JSON
 stream.on('data', data => {
-  console.log(data);
+  // emit data to all connected clients
+// console.log(data);
+  io.emit('caption', data);
 });
 stream.on('end', function () {
   console.log("End of Stream");
@@ -49,9 +62,6 @@ stream.on('end', function () {
 const mic = require('mic');
 
 // initialize microphone configuration
-// note: microphone device id differs
-// from system to system and can be obtained with
-// arecord --list-devices and arecord --list-pcms
 const micConfig = {
     /* sample rate */ rate: 16000,
     /* channels */    channels: 1,
@@ -73,5 +83,7 @@ micStream.pipe(stream);
 // start the microphone
 micInstance.start();
 
-// Forcibly ends the streaming session
-// stream.end();
+// start server
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
